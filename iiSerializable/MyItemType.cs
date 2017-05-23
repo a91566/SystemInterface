7b9039208e8e6279a6dda12d1754b1641802977f
zhengShaobao <a91566@163.com>
2017-05-24 07:31:07 +0800

    当你孤单你会想起谁

---
+++ iiSerializable/MyItemType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iiSerializable
+{
+	[Serializable]
+	public class MyItemType: ISerializable
+	{
+		public MyItemType()
+		{
+			// Empty constructor required to compile.
+		}
+		
+		// The special constructor is used to deserialize values.
+		public MyItemType(SerializationInfo info, StreamingContext context)
+		{
+			// Reset the property value using the GetValue method.
+			School = (string)info.GetValue("school", typeof(string));
+			Name = (string)info.GetValue("name", typeof(string));
+			Age = (string)info.GetValue("age", typeof(string));
+		}
+
+		// The value to serialize.
+		public string Name { get; set; }
+		public string Age { get; set; }
+		public string School { get; set; }
+
+		// Implement this method to serialize data. The method is called 
+		// on serialization.
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			// Use the AddValue method to specify serialized values.
+			info.AddValue("school", School, typeof(string));
+			info.AddValue("name", Name, typeof(string));
+			info.AddValue("age", Age, typeof(string));
+
+		}
+
+	}
+}
---
+++ iiSerializable/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace iiSerializable
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// This is the name of the file holding the data. You can use any file extension you like.
+			string fileName = "dataStuff.myData";
+
+			// Use a BinaryFormatter or SoapFormatter.
+			IFormatter formatter = new BinaryFormatter();
+			//IFormatter formatter = new SoapFormatter();
+
+			Program.SerializeItem(fileName, formatter); // Serialize an instance of the class.
+			Program.DeserializeItem(fileName, formatter); // Deserialize the instance.
+
+			Console.WriteLine("Done");
+			Console.ReadLine();
+		}
+
+		public static void SerializeItem(string fileName, IFormatter formatter)
+		{
+			// Create an instance of the type and serialize it.
+			MyItemType t = new MyItemType();
+			t.Name = "Hello World";
+			t.School = "qinghua";
+			t.Age = "18";
+
+			FileStream s = new FileStream(fileName, FileMode.Create);
+			formatter.Serialize(s, t);
+			s.Close();
+		}
+
+
+		public static void DeserializeItem(string fileName, IFormatter formatter)
+		{
+			FileStream s = new FileStream(fileName, FileMode.Open);
+			MyItemType t = (MyItemType)formatter.Deserialize(s);
+			Console.WriteLine(t.Name);
+			Console.WriteLine(t.School);
+			Console.WriteLine(t.Age);
+		}
+	}
+}
